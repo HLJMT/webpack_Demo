658d659772969da7c02bd01570eec618eeecf5c0
HLJMT <harlem097@163.com>
2019-04-20 17:49:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# webpack_demo
+
+## 安装Webpack
+```
+npm install --save-dev webpack
+```
+
+### 项目启动/编译打包
+```
+npm start
+```
---
+++ app/Greeter.js
@@ -0,0 +1,5 @@
+module.exports = function () {
+    let str = document.createElement('div');
+    str.textContent = 'Hello word! ';
+    return str
+}
